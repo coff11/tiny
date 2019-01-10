e55001c4886bf546a9b063dc85b59152e25fcb17
Ray <aaxx.2xx@gmail.com>
2019-01-10 15:02:52 +0800

    refactor
    增加了代码注释，封装了选择器函数

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,20 @@
+/*
+ * 渲染进程 
+ */
+
 const {
   ipcRenderer
 } = require('electron')
-const minBtn = document.querySelector('.min-btn')
-const maxBtn = document.querySelector('.max-btn')
-const shutBtn = document.querySelector('.shut-btn')
 
-const imgListEle = document.querySelector('.img-list')
-let imgList = []
-let totalSizeStart = 0
-const fileInputEle = document.getElementById('file-input')
-let imgTpl = []
-let pressNum = 0
+const getIdEle = obj => document.getElementById(obj)
+const getQueryEle = obj => document.querySelector(obj)
 
+/*
+ * 右上角菜单最小化，全屏，关闭
+ */
+const minBtn = getQueryEle('.min-btn')
+const maxBtn = getQueryEle('.max-btn')
+const shutBtn = getQueryEle('.shut-btn')
 minBtn.addEventListener('click', () => {
   ipcRenderer.send('min')
 })
@@ -22,29 +25,37 @@ shutBtn.addEventListener('click', () => {
   ipcRenderer.send('shut')
 })
 
+/*
+ * imgListEle: 压缩结果的展示列表
+ * fileInputEle: 上传控件
+ * imgList: 用于保存图片的相关信息
+ * totalSizeStart: 压缩之前图片的总体积
+ * pressNum: 已经压缩的图片数量
+ */
+const imgListEle = getQueryEle('.img-list')
+const fileInputEle = getIdEle('file-input')
+let imgList = []
+let totalSizeStart = 0
+let pressNum = 0
+
 fileInputEle.onchange = () => {
+  /*
+   * 清空数据，重置界面
+   */
   pressNum = 0
   imgListEle.innerHTML = ''
   totalSizeStart = 0
   imgList = []
+
+  
   for(let i = 0; i < fileInputEle.files.length; i ++) {
-    document.getElementById('total').innerText = fileInputEle.files.length
-    document.getElementById('now').innerText = 0
-    document.querySelector('.loading-wrapper').classList.remove('hide')
-    document.querySelector('.cover').classList.remove('hide')
+    getIdEle('total').innerText = fileInputEle.files.length
+    getIdEle('now').innerText = 0
+    getQueryEle('.loading-wrapper').classList.remove('hide')
+    getQueryEle('.cover').classList.remove('hide')
     totalSizeStart += parseInt(fileInputEle.files[i].size / 1024)
-    document.getElementById('num-start').innerText = totalSizeStart
-    document.getElementById('num-end').innerText = '?'
-    imgTpl.push({
-      id: fileInputEle.files[i].name,
-      tpl: `
-        <li>
-          <span class="show-name">${fileInputEle.files[i].name}</span>&nbsp;&nbsp;&nbsp;
-        </li>
-      `,
-      size: fileInputEle.files[i].size,
-      class: ''
-    })
+    getIdEle('num-start').innerText = totalSizeStart
+    getIdEle('num-end').innerText = '?'
     imgList.push({
       path: fileInputEle.files[i].path,
       name: fileInputEle.files[i].name,
@@ -52,62 +63,85 @@ fileInputEle.onchange = () => {
       dirname: fileInputEle.files[i].path.replace(fileInputEle.files[i].name, '')
     })
   }
+  /*
+   * 将保存了图片相关信息的数组传递给主进程
+   */
   ipcRenderer.send('imgList', imgList)
 }
 
+/*
+ * 接收压缩后的图片总体积
+ */
 ipcRenderer.on('totalEnd', (e, data) => {
-  document.getElementById('num-end').innerText = data
+  getIdEle('num-end').innerText = data
 })
 
+/*
+ * 每一张图片压缩完成的响应事件
+ */
 ipcRenderer.on('complete', (e, data) => {
   imgListEle.innerHTML += data
   pressNum += 1
-  document.getElementById('now').innerText = pressNum
+  getIdEle('now').innerText = pressNum
   if(pressNum == fileInputEle.files.length) {
-    document.querySelector('.loading-wrapper').classList.add('hide')
-    document.querySelector('.cover').classList.add('hide')
+    getQueryEle('.loading-wrapper').classList.add('hide')
+    getQueryEle('.cover').classList.add('hide')
   }
 })
 
-document.querySelector('.key-text').value = localStorage.getItem('key')
+/*
+ * 从localStorage中读取key值，显示在输入框 
+ */
+getQueryEle('.key-text').value = localStorage.getItem('key')
 
-document.querySelector('.setting').addEventListener('click', () => {
-  document.querySelector('.key-wrapper').classList.remove('hide')
-  document.querySelector('.key-text').value = window.localStorage.getItem('key')
+getQueryEle('.setting').addEventListener('click', () => {
+  getQueryEle('.key-wrapper').classList.remove('hide')
+  getQueryEle('.key-text').value = window.localStorage.getItem('key')
 })
-document.querySelector('.confirm-btn').addEventListener('click', () => {
-  const keyValue = document.querySelector('.key-text').value
+
+/*
+ * 用户输入key值后，保存到localStorage，并发给主进程校验 
+ */
+getQueryEle('.confirm-btn').addEventListener('click', () => {
+  const keyValue = getQueryEle('.key-text').value
   window.localStorage.setItem('key', keyValue)
   ipcRenderer.send('key', keyValue)
-  document.querySelector('.loading-wrapper').classList.remove('hide')
-  document.querySelector('.cover').classList.remove('hide')
+  getQueryEle('.loading-wrapper').classList.remove('hide')
+  getQueryEle('.cover').classList.remove('hide')
 })
 
-document.querySelector('.cancel-btn').addEventListener('click', () => {
-  document.querySelector('.key-wrapper').classList.add('hide')
+getQueryEle('.cancel-btn').addEventListener('click', () => {
+  getQueryEle('.key-wrapper').classList.add('hide')
 })
 
+/*
+ * key值校验结果由主进程判断是否抛错，渲染进程只管理loading和输入界面的显示与否 
+ */
 ipcRenderer.on('keyIsOk', () => {
-  document.querySelector('.key-wrapper').classList.add('hide')
-  document.querySelector('.loading-wrapper').classList.add('hide')
-  document.querySelector('.cover').classList.add('hide')
+  getQueryEle('.key-wrapper').classList.add('hide')
+  getQueryEle('.loading-wrapper').classList.add('hide')
+  getQueryEle('.cover').classList.add('hide')
 })
 ipcRenderer.on('keyIsErr', () => {
-  document.querySelector('.loading-wrapper').classList.add('hide')
-  document.querySelector('.cover').classList.add('hide')
+  getQueryEle('.loading-wrapper').classList.add('hide')
+  getQueryEle('.cover').classList.add('hide')
 })
+
+/*
+ * 错误处理
+ */
 ipcRenderer.on('errHandler', (e, data) => {
-  document.querySelector('.loading-wrapper').classList.add('hide')
-  document.querySelector('.cover').classList.add('hide')
+  getQueryEle('.loading-wrapper').classList.add('hide')
+  getQueryEle('.cover').classList.add('hide')
   alert(data)
   pressNum = 0
   imgListEle.innerHTML = ''
   totalSizeStart = 0
   imgList = []
   fileInputEle.value = ''
-  document.getElementById('total').innerText = fileInputEle.files.length
-  document.getElementById('now').innerText = 0
-  document.getElementById('num-start').innerText = totalSizeStart
-  document.getElementById('num-end').innerText = '?'
+  getIdEle('total').innerText = fileInputEle.files.length
+  getIdEle('now').innerText = 0
+  getIdEle('num-start').innerText = totalSizeStart
+  getIdEle('num-end').innerText = '?'
 })
 
--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+/*
+ * 主进程
+ */
+
 const {
   app,
   BrowserWindow,
@@ -9,6 +13,9 @@ const fs = require('fs')
 let win
 let totalSizeEnd = 0
 
+/*
+ * key值校验 
+ */
 ipcMain.on('key', (e, data) => {
   tinify.key = data
   tinify.validate(function(err) {
@@ -18,10 +25,12 @@ ipcMain.on('key', (e, data) => {
     } else {
       win.webContents.send('keyIsOk')
     }
-    // Validation of API key failed.
   })
 })
 
+/*
+ * 初始化app
+ */
 function initApp() {
   win = new BrowserWindow({
     width: 1000,
@@ -42,18 +51,26 @@ function initApp() {
   ipcMain.on('shut', () => {
     app.quit()
   })
+  /*
+   * 压缩图片
+   */
   function pressImg(list) {
     for(let i = 0; i < list.length; i ++) {
       fs.readFile(list[i].path, (err, data) => {
         if(err) {
           console.log(err)
         } else {
+          /*
+           * 调用tinyPNG的压缩方法，每一步返回的都是一个promise对象
+           */
           const source = tinify.fromFile(list[i].path)
           const result = source.toFile(list[i].path)
+          /*
+           * 获取压缩之后的图片信息 
+           */
           result.then((x) => {
             fs.stat(list[i].path, (err, data) => {
               totalSizeEnd += parseInt(data.size / 1024)
-              console.log(list[i].name)
               win.webContents.send('totalEnd', totalSizeEnd, i)
               win.webContents.send('complete', `
                 <li class="success">
@@ -62,29 +79,32 @@ function initApp() {
               `)
             })
           }).catch((err) => {
+            /*
+             * 错误处理 
+             */
             if (err instanceof tinify.AccountError) {
+              // Verify your API key and account limit.
               win.webContents.send('errHandler', 'Verify your API key and account limit.')
               throw err
-              // Verify your API key and account limit.
             } else if (err instanceof tinify.ClientError) {
+              // Check your source image and request options.
               win.webContents.send('complete', `
                 <li class="failed">
                   <span class="show-name">${list[i].name}</span>&nbsp;&nbsp;&nbsp;
                 </li>
               `)
-              // Check your source image and request options.
             } else if (err instanceof tinify.ServerError) {
               win.webContents.send('errHandler', 'Temporary issue with the Tinify API.')
               throw err
               // Temporary issue with the Tinify API.
             } else if (err instanceof tinify.ConnectionError) {
+              // A network connection error occurred.
               win.webContents.send('errHandler', 'A network connection error occurred.')
               throw err
-              // A network connection error occurred.
             } else {
+              // Something else went wrong, unrelated to the Tinify API.
               win.webContents.send('errHandler', 'Something else went wrong, unrelated to the Tinify API.')
               throw err
-              // Something else went wrong, unrelated to the Tinify API.
             }
           })
         }
@@ -98,19 +118,3 @@ function initApp() {
 }
 
 app.on('ready', initApp)
-
-
-
-// app.on('window-all-closed', () => {
-//   if (process.platform !== 'darwin') {
-//     app.quit()
-//   }
-// })
-
-// app.on('activate', () => {
-//   if(win === null) {
-//     createWindow()
-//   }
-// })
-
-
